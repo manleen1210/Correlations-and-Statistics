531dc37a9f3ba4ddfe9130996943eed6de9d03e2
Manleen Kaur Chhabra <manleen.chhabra@stonybrook.edu>
2021-10-28 04:42:44 +0400

    Added text edits
    Human readable interpretations included

--- Assignment3_Ttest_507.py
+++ Assignment3_Ttest_507.py
@@ -14,7 +14,7 @@ Looking at a small sample
 """
 diabetes_small = diabetes.sample(100)
 
-### Getting a list of all variabke names from our small sample ### 
+"""Getting a list of all variabke names from our small sample"""  
 list(diabetes_small)
 
 """
@@ -27,6 +27,8 @@ Questions to be answered
 """ Question 1
 1) Is there a difference between sex (M:F) and the number of days in hospital? """
 
+""" Creating new dataframes to align with each level of the sex column """
+
 Female = diabetes[diabetes['gender']=='Female']
 Male = diabetes[diabetes['gender']=='Male']
 
@@ -36,7 +38,11 @@ from scipy.stats import ttest_1samp
 
 
 ttest_ind(Female['time_in_hospital'], Male['time_in_hospital'])
-""" p-value here is 1.4217299655114968e-21 and t-test statistic = 9.542637042242887 """
+
+""" p-value here is 1.4217299655114968e-21 and t-test statistic = 9.542637042242887
+The p-value is << 0.05 which indicates that the data is significant and a high 
+statistic value indicates that there is a difference between 
+the male and female data for number of days in hospital """
 
 
 
@@ -47,12 +53,20 @@ ttest_ind(Female['time_in_hospital'], Male['time_in_hospital'])
  Is there a difference between RACE (Caucasian and African American) 
  and the number of days in hospital? """
  
+ """ Creating new dataframes to align with each level of the race column 
+ as specified in the question """
+ 
  Caucasian = diabetes[diabetes['race']=='Caucasian']
  AfricanAmerican = diabetes[diabetes['race']=='AfricanAmerican']
  
  """T-test"""
  ttest_ind(Caucasian['time_in_hospital'], AfricanAmerican['time_in_hospital'])
- """ p-value here is 4.178330085585203e-07 and t-test statistic = 5.0610017032095325 """
+ 
+ """ p-value here is 4.178330085585203e-07 and t-test statistic = -5.0610017032095325
+The p-value is << 0.05 which indicates that the data is significant and 
+a high absolute statistic value indicates that the data is significantly different
+between the Caucasian and AfricanAmerican data for number of days in hospital
+"""
  
  
  
@@ -63,11 +77,18 @@ ttest_ind(Female['time_in_hospital'], Male['time_in_hospital'])
  Is there a difference between RACE (Asian and African American) and the number
  of lab procedures performed? """
  
+""" Creating one new dataframe to align with each level of the race column 
+ as specified in the question since the dataframe for AfricanAmerican 
+ individuals has already been created """
  
  Asian =  diabetes[diabetes['race']=='Asian']
   
  """T-test"""
  ttest_ind(Asian['num_lab_procedures'], AfricanAmerican['num_lab_procedures'])
- """ p-value here is 6.948907528800307e-05 and t-test statistic = -3.9788715315360292 """
  
+ """ p-value here is 6.948907528800307e-05 and t-test statistic = -3.9788715315360292 
+ The p-value is << 0.05 which indicates that the data is significant and 
+ a high absolute statistic value indicates that the data is significantly different
+for Asian and AfricanAmerican data for number of lab procedures performed """
  
+
